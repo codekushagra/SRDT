48dd7810d87a3749b7cbbb85fdebcc96084973b7
KUSHAGRA CHATURVEDI <118090317+codekushagra@users.noreply.github.com>
2025-05-27 11:31:48 +0530

    array updated

--- Day2_Array.java
+++ Day2_Array.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Day2_Array {
     
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[10];
+        int sum=0;
+        float avg;
+
+        for(int i=0;i<a.length;i++){
+            a[i]=sc.nextInt();
+
+        }
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+        System.out.println(sum);
+        System.out.println(avg=sum/10);
+    }
 }
