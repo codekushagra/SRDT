2f8d018be425c30170e7add1c56f42b018bf7b91
KUSHAGRA CHATURVEDI <118090317+codekushagra@users.noreply.github.com>
2025-05-27 10:01:34 +0530

    initial commit

---
+++ first.java
@@ -0,0 +1,12 @@
+public class first {
+    public static void main(String[] args) {
+        int n=10;
+        if(n%2==0){
+            System.out.println("Number is even");
+        }else{
+            System.out.println(
+            "Number is odd"
+            );
+        }
+    }
+}
