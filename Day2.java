89feb06beac928d1d3e4c735200cbe83de35af21
KUSHAGRA CHATURVEDI <118090317+codekushagra@users.noreply.github.com>
2025-05-27 10:10:09 +0530

    day2

--- Day2.java
+++ Day2.java
@@ -1,12 +1,10 @@
-public class first {
+public class Day2 {
     public static void main(String[] args) {
         int n=10;
         if(n%2==0){
             System.out.println("Number is even");
         }else{
-            System.out.println(
-            "Number is odd"
-            );
+            System.out.println("Number is odd" );
         }
     }
 }
